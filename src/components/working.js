a849f6b04f2b80a21127418edbdef8b71535deab
ArshiqIqbal <108603169+ArshiqIqbal@users.noreply.github.com>
2022-07-16 17:30:49 +0500

    Add files via upload
    My First React Web-Page

---
+++ src/components/working.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export default function working() {
+  return (
+    <>
+    <section data-bs-version="5" class="features7 cid-sHbZFN6Ckx" id="features07-4">
+    
+    
+    <div class="container">
+
+        <div class="row justify-content-center row-bg mt-4">
+            <div class="mbr-section-head col-12 pb-4">
+                <h4 class="mbr-section-title mbr-fonts-style align-left pt-2 mb-0 display-5">How it works?</h4>
+                
+            </div>
+
+            <div class="сol-12 md-pb col-md-12 col-lg-6">
+                <div class="card item-wrapper">
+
+                    <div class="item-img">
+                    
+                    </div>
+                    <div class="item-content">
+                        <h5 class="item-title align-left mt-4 mb-3 mbr-fonts-style display-2">Unique T-Shirt</h5>
+                    </div>
+
+                    <div class="mbr-section-btn pt-4 mb-0 align-right"><a href="" class="btn item-btn btn-black-outline display-7" target="_blank">Buy for $59</a> <a href="" class="btn item-btn btn-primary display-7" target="_blank"><span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span>View all</a></div>
+
+                </div>
+            </div>
+
+            <div class="сol-12 col-md-12 col-lg-6">
+                <div class="card item-wrapper">
+                    <div class="item-content">
+                        <h5 class="item-title title-border align-left mb-4 mbr-fonts-style display-7">Unique T-Shirt Design</h5>
+
+                        <p class="mbr-text align-left mt-4 mb-0 mbr-fonts-style display-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id orci nec lectus lacinia tempus. Praesent nec quam mollis turpis eleifend consequat. <br></br>Phasellus euismod maximus ipsum vehicula vehicula. Mauris posuere risus nec purus dapibus semper sed id ligula. Integer massa lorem, fringilla vel eros.</p>
+                    </div>
+                    <div class="item-content">
+                        
+
+
+                        <div class="mbr-section-btn pt-4 mb-0 align-right"><a href="" class="btn item-btn btn-black-outline display-7" target="_blank">Subscribe $79/q</a> <a href="" class="btn item-btn btn-primary display-7" target="_blank"><span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span>More
+                                Info</a></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+    </>
+  )
+}
